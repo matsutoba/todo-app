1eef560c0ef6aa1400f6c1e82dedd462fb729565
Toshiyuki Matsubara <matsubara_t@outlook.jp>
2024-12-30 16:03:57 +0900

    環境変数のロード処理

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"log"
+	"todo-app/internal/config"
 )
 
 func main() {
 	log.Print("Server start.")
+	config.LoadEnv()
+	env := config.GetEnv("ENV", "")
+	log.Print("Environment: ", env)
 }
