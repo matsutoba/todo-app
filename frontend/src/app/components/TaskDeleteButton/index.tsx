cc286e63308953490963a3fb52d4e4e5bbcc64e7
Toshiyuki Matsubara <matsubara_t@outlook.jp>
2025-02-08 15:08:10 +0900

    TODO削除処理の追加

--- frontend/src/app/components/TaskDeleteButton/index.tsx
+++ frontend/src/app/components/TaskDeleteButton/index.tsx
@@ -1,16 +1,50 @@
-import { FaTrashAlt } from "react-icons/fa";
+"use client";
 
+import { FaTrashAlt } from "react-icons/fa";
+import { useActionState, useState, useTransition } from "react";
+import { deleteTodo } from "@/app/actions/todo";
+import ConfirmModal from "../ConfirmModal";
 interface TaskDeleteButtonProps {
   id: number;
 }
 
 const TaskDeleteButton: React.FC<TaskDeleteButtonProps> = ({ id }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [, startTransitionq] = useTransition();
+
+  const deleteTodoWithId = deleteTodo.bind(null, id);
+  const [, formAction, isPending] = useActionState(deleteTodoWithId, {
+    error: "",
+  });
+
+  const handleDelete = async () => {
+    startTransitionq(async () => {
+      await formAction();
+    });
+  };
+
   return (
-    <form action="">
-      <button type="submit">
-        <FaTrashAlt className="hover:text-gray-400 text-lg cursor-pointer size-4" />
+    <>
+      <button
+        disabled={isPending}
+        onClick={() => setIsOpen(true)}
+        className="hover:text-gray-400 text-lg cursor-pointer size-4 disabled:opacity-50"
+      >
+        <FaTrashAlt />
       </button>
-    </form>
+      {isOpen && (
+        <ConfirmModal
+          title="タスクを削除しますか？"
+          message="この操作は元に戻すことができません。"
+          isOpen={isOpen}
+          onCancel={() => setIsOpen(false)}
+          onConfirm={() => {
+            setIsOpen(false);
+            handleDelete();
+          }}
+        />
+      )}
+    </>
   );
 };
 
