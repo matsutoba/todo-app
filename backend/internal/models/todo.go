4afddc6eb5055cba1099c07b32442ad68f810f12
Toshiyuki Matsubara <matsubara_t@outlook.jp>
2025-02-08 12:57:07 +0900

    レスポンスのJSONをキャメルケースにしたいので、gormModelを使わずにid, createdAt, updatedAtを定義する。due_dateをキャメルケースに修正する、

--- backend/internal/models/todo.go
+++ backend/internal/models/todo.go
@@ -2,14 +2,14 @@ package models
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type Todo struct {
-	gorm.Model
+	ID          uint      `json:"id" gorm:"primaryKey"`
 	Title       string    `gorm:"size:255;not null" json:"title"` // タスクのタイトル
 	Description string    `gorm:"type:text" json:"description"`   // タスクの詳細（オプション）
 	Completed   bool      `gorm:"default:false" json:"completed"` // 完了フラグ
-	DueDate     time.Time `json:"due_date"`                       // 締め切り日時（オプション）
+	DueDate     time.Time `json:"dueDate"`                        // 締め切り日時（オプション）
+	CreatedAt   int64     `json:"createdAt"`
+	UpdatedAt   int64     `json:"updatedAt"`
 }
